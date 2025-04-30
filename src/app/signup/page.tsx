259fc8a2baeca4e28adca7589edbf512a46ab7cc
itssoumodip <soumodipddas2005@gmail.com>
2025-04-30 17:57:06 +0530

    SignupPage layout and implement user state management

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -6,9 +6,53 @@ import { Axios } from "axios";
 
 
 export default function SignupPage() {
+  const [user, setUser] = React.useState({
+    email: "",
+    password: "",
+    username: "",
+  });
+
+  const onSignup = async () => {}
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1 className="">Signup</h1>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 font-[family-name:var(--font-geist-sans)]">
+      <h1>Signup</h1>
+      <hr />
+      <label htmlFor="username">Username</label>
+      <input 
+        id="username"
+        type="text"
+        value={user.username}
+        onChange={(e) => setUser({ ...user, username: e.target.value })}
+        placeholder="Username"
+        className="border-2 border-gray-300 rounded-md p-2 mb-4 w-1/3">
+      </input>
+      <label htmlFor="email">email</label>
+      <input 
+        id="email"
+        type="text"
+        value={user.email}
+        onChange={(e) => setUser({ ...user, email: e.target.value })}
+        placeholder="email"
+        className="border-2 border-gray-300 rounded-md p-2 mb-4 w-1/3">
+      </input>
+      <label htmlFor="password">password</label>
+      <input 
+        id="password"
+        type="password"
+        value={user.password}
+        onChange={(e) => setUser({ ...user, password: e.target.value })}
+        placeholder="password"
+        className="border-2 border-gray-300 rounded-md p-2 mb-4 w-1/3">
+      </input>
+      <button
+        className="bg-gray-600 text-white rounded-md p-2 w-1/3 hover:bg-gray-700"
+        onClick={onSignup}
+        >
+          Signup here
+      </button>
+      <Link href="/login" className="text-gray-400 hover:underline mt-4">
+        Visit Login page 
+      </Link>
     </div>
   );
 }   
