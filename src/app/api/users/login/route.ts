56d2640054b801ff949074a18e0fdb1ab0b1f031
itssoumodip <soumodipddas2005@gmail.com>
2025-04-30 20:28:47 +0530

    Add JWT authentication to login route and enhance LoginPage functionality

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -2,13 +2,43 @@ import { connectDB } from "@/dbConfig/dbConfig";
 import User from "@/models/userModel";
 import { NextRequest, NextResponse } from "next/server";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 connectDB();
 export async function POST(req: NextRequest) { 
     try {
         const reqBody = await req.json();
         const {email, password} = reqBody;
-        
+
+        // check if user exists
+        const user = await User.findOne({email});
+        if (!user) {
+            return NextResponse.json({error: "User not found"}, {status: 400});
+        }
+        // check if password is correct
+        const validPassword = await bcrypt.compare(password, user.password);
+        if (!validPassword) {
+            return NextResponse.json({error: "Invalid password"}, {status: 400});
+        }
+        // create token data
+        const tokenData = {
+            id: user._id,
+            email: user.email,
+            username: user.username
+        };
+        // create token
+        const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET!, {expiresIn: "1d"});
+
+        const response = NextResponse.json({
+            message: "Login successful",
+            success: true,
+        })
+
+        response.cookies.set("token", token, {
+            httpOnly: true,
+        })
+        return response;
+
     } catch (error: any) {
         console.error("Login error details:", error);
         return NextResponse.json({
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,20 +1,48 @@
 "use client";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import axios  from "axios";
+import toast from "react-hot-toast";
 
 
 export default function LoginPage() {
+  const router = useRouter();
+  const [loading, setLoading] = React.useState(false);
   const [user, setUser] = React.useState({
     email: "",
     password: "",
   });
+  const [buttonDisabled, setButtonDisabled] = React.useState(false);
+  
 
-  const onSignup = async () => {}
+  const onSignup = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.post("/api/users/login", user);
+      console.log("Login success", response.data);
+      toast.success("Login successful, redirecting to home page");
+      router.push("/profile");
+    } catch (error: any) {
+      console.log("Error signing up", error.message);
+      toast.error("Error signing up, please try again later");
+    } finally {
+      setLoading(false);
+    }
+  }
+  
+  useEffect(() => {
+    if(user.email.length > 0 && user.password.length > 0) {
+      setButtonDisabled(false);
+    }
+    else {
+      setButtonDisabled(true);
+    }
+  }, [user])
+  
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2 font-[family-name:var(--font-geist-sans)]">
-      <h1>Login</h1>
+      <h1>{loading ? "processing..." : "login"}</h1>
       <hr />
       <label htmlFor="email">email</label>
       <input 
